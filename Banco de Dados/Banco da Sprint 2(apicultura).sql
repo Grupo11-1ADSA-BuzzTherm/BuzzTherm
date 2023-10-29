fa0143727d096e48ad908fc0ced247252f2eab51
Fabricio-S-Fagundes <fabricio.fagundes@sptech.school>
2023-10-29 16:31:31 -0300

    Revisão do script de BD

--- Banco de Dados/Banco da Sprint 2(apicultura).sql
+++ Banco de Dados/Banco da Sprint 2(apicultura).sql
@@ -1,15 +1,22 @@
-create database sprint2;
+create database bdBuzzTherm;
 
-use sprint2;
+use bdBuzzTherm;
 
 create table empresa (
 idEmpresa int primary key auto_increment,
-nome varchar(100) not null,
+razaoSocial varchar(100) not null,
+nomeFantasia varchar(100) not null,
 cnpj char(14)not null,
+enderecoEmpresa varchar(255) not null,
+uf varchar(2) not null,
 email varchar(80)not null,
+telefone1 char(11),
+telefone2 char(11),
 qtdColmeia int not null,
 planoAtual varchar(45)
-constraint chkplan check(planoAtual in("Mensal","Semestral","Anual"))
+constraint chkplan check(planoAtual in("Mensal","Semestral","Anual")),
+fkFilial int not null ,
+foreign key (fkFilial) references empresa(idEmpresa)
 )auto_increment=1000;
 
 desc empresa;
@@ -19,230 +26,180 @@ idFuncionario int primary key auto_increment,
 login varchar(255)not null,
 senha varchar(45)not null,
 cargo varchar(45)not null,
-fkEmpresa int,
+fkEmpresa int not null,
 foreign key(fkEmpresa) references empresa(idEmpresa)
 );
 
 desc funcionario;
 
-create table telefone(
-idTelefone int primary key auto_increment,
-telefone1 char(11),
-telefone2 char(11),
-telefone3 char(11),
+create table setor(
+idSetor int primary key auto_increment,
+nomeFazenda varchar(255),
 fkEmpresa int,
 foreign key(fkEmpresa) references empresa(idEmpresa)
 );
 
-desc telefone;
-
-create table endereco(
-idEndereco int primary key auto_increment,
-cep char(8),
-lograduro varchar(255),
-numero int,
-fkEmpresa int,
-foreign key(fkEmpresa) references empresa(idEmpresa)
-);
-
-desc endereco;
+desc setor;
 
 create table colmeia(
-idColmeia int primary key auto_increment,
-tamanho char(3) not null,
+idColmeia int auto_increment,
+qtdQuadros int not null,
 tipoColmeia varchar(50)not null,
-fkEmpresa int,
-foreign key(fkEmpresa) references empresa(idEmpresa)
-)auto_increment=100;
+fkSetor int not null,
+foreign key(fkSetor) references setor(idSetor),
+primary key (idColmeia,fkSetor)
+)auto_increment=10;
 
 desc colmeia;
 
 create table sensorDHT11(
-idSensor int auto_increment,
+idSensor int primary key auto_increment,
+situacao tinyint,
 fkColmeia int,
 foreign key(fkColmeia) references colmeia(idColmeia),
-situacao tinyint,
-primary key(idSensor, fkColmeia)
+fkSetor int,
+foreign key (fkSetor) references setor(idSetor)
 );
 
 desc sensorDHT11;
 
 
 create table registro(
-idRegistro int auto_increment,
+idRegistro int primary key auto_increment,
 dataHora datetime,
 temp float not null,
 umid float not null,
-fkSensor int,
-foreign key (fkSensor) references sensorDHT11(idSensor),
-primary key(idRegistro, fkSensor)
+fkSensor int not null,
+foreign key (fkSensor) references sensorDHT11(idSensor)
 );
 
 desc registro;
 
+alter table empresa modify column fkFilial int null;
+
 show tables;
 
--- a medida utilizada foi de mil metros quadrados ( 42.72km² = 42.720m² de area)
+insert into empresa(idEmpresa,razaoSocial,nomeFantasia,cnpj,enderecoEmpresa,uf,email,telefone1,telefone2,qtdColmeia,planoAtual)values
+(null, 'Empires Bee Industria LTDA','Empires Bee', '61696740000107', 'Avenida Paulista, 39', 'SP', 'empires.bee@gmail.com', '11984568569' , '11986653365', 450, 'Semestral'),
+(null, 'Empires Bee Industria LTDA','Empires To Bee', '00090625000191', 'Santo Amaro, 340', 'SP', 'tobeecorporation@live.com', '11986547953', '11985447442', 395, 'Semestral'),
+(null, 'BeeMaid Honey Agronégocio LTDA','BeeMaid Honey', '17942850000101', 'Alameda Virturiosa, 1300', 'RJ', 'beemaidhoney@bol.com.br', '21975621355', '21957466621', 420, 'Anual'),
+(null, 'BeeMaid Honey Agronégocio LTDA','beeMaid Honey Acres', '30610441000176', 'Rua Dom Pedro II, 804', 'RJ', 'honeyacres@hotmail.com', '21954623348', '21954785981', 339,'Anual');
 
-insert into empresa values
-(null, 'Empires Bee', '61696740000107', 'empires.bee@gmail.com', '450', 'Semestral'),
-(null, 'To Bee', '00090625000191', 'tobeecorporation@live.com', '395', 'Mensal'),
-(null, 'BeeMaid Honey', '17942850000101', 'beemaidhoney@bol.com.br', '420', 'Anual'),
-(null, 'Honey Acres', '30610441000176', 'honeyacres@hotmail.com', '339', 'Semestral');
+select * from empresa; 
+
+update empresa set fkFilial = 1000
+where idEmpresa = 1001;
+
+update empresa set fkFilial = 1002
+where idEmpresa = 1003;
 
 -- use para mostrar o erro do check
-insert into empresa values
-(null, 'Empires', '61', 'empires@gmail.com', '45', 'Semanal');
+-- insert into empresa values
+-- (null, 'Empires', '61', 'empires@gmail.com', '45', 'Semanal');
 
--- parei aqui
+insert into funcionario values 
+(null, 'marcio.rocha12', '4523', 'Vice-Presidente', 1000),
+(null, 'ana.maria23', '6543', 'Presidente', 1000),
+(null, 'carlos.henrique90', '9817', 'Supervisor Lógistico', 1003),
+(null, 'yasmin.raviero34', '3258', 'Supervisora', 1003),
+(null, 'pedro.salvatore98', '4789', 'Gerente Adminstrativo', 1001),
+(null, 'maria.silva87', '3214', 'Gerente Administrativa', 1002);
 
+select * from funcionario;
 
+select 
+funcionario.login,
+Empresa.nomeFantasia
+from funcionario
+join empresa as Empresa
+on funcionario.fkEmpresa = Empresa.idEmpresa
+left join empresa as filial
+on filial.fkFilial = Empresa.idEmpresa;
 
+insert into setor values
+(null, "Fazenda da Terra Doce", 1002),
+(null, "Fazenda Cavalcante", 1000),
+(null, "Fazenda Terra Fértil", 1001),
+(null, "Fazenda do Vale Dourado", 1003);
 
+select * from setor;
 
-
-select * from empresa;
-
-insert into usuario values 
-(null, 'marcio.rocha12', '4523', 'Vice-Presidente'),
-(null, 'yasmin.raviero34', '3258', 'Supervisora'),
-(null, 'pedro.salvatore98', '4789', 'Gerente Adminstrativo'),
-(null, 'maria.silva87', '3214', 'Gerente Administrativa');
-
-select * from usuario;
-
--- a unidade de medida utilizada foi de centrimetros cubicos ex(50055.60cm3 = 50.5 * 41.3 * 24.4)
+select * from setor 
+join empresa as Empresa
+on setor.fkEmpresa = Empresa.idEmpresa
+left join empresa as Filial
+on Filial.fkFilial = Empresa.idEmpresa;
 
 insert into colmeia values
-(null, '50055.60', 'Uruçu'),
-(null, '49254.45', 'Europeia'),
-(null, '45157.50', 'Africanizada'),
-(null, '36457.59', 'Jandaíra');
+(null, '7', 'ninho',1),
+(null, '6', 'melgueira',2),
+(null, '7', 'melgueira rasa',4),
+(null, '8', 'ninho',3),
+(null, '9', 'melgueira',1),
+(null, '7', 'melgueira',1),
+(null, '8', 'melgueira rasa',4);
 
 select * from colmeia;
 
+select * from colmeia
+join setor
+on fkSetor = idSetor
+join empresa
+on fkEmpresa = idEmpresa;
+
 insert into sensorDHT11 values
-(null),
-(null),
-(null),
-(null),
-(null),
-(null),
-(null),
-(null);
+(null, 1, 10, 1),
+(null, 1, 12, 4),
+(null, 1, 14, 1),
+(null, 1, 13, 3),
+(null, 0, 15, 1),
+(null, 1, 16, 4),
+(null, 1, 11, 2);
 
 select * from sensorDHT11;
+
+select * from sensorDHT11 as sensor
+join colmeia
+on fkColmeia = idColmeia
+inner join setor
+on sensor.fkSetor = setor.idSetor;
 
 -- o ultimo valor inserido é lido em porcentagem
 
 insert into registro values
-(null, '2023-09-23 08:54:48', '23.4', '54.5'),
-(null, '2023-09-23 10:40:30', '29.0', '60'),
-(null, '2023-09-24 09:54:48', '28.3', '68.2'),
-(null, '2023-09-24 11:04:20', '22.1', '45.8'),
-(null, '2023-09-23 6:00:39', '25.4', '40'),
-(null, '2023-09-23 08:30:00', '25.4', '64.6'),
-(null, '2023-09-24 07:45:29', '20', '50'),
-(null, '2023-09-24 08:45:42', '26', '68.2');
+(null, '2023-09-23 08:54:48', '23.4', '54.5',1),
+(null, '2023-09-23 10:40:30', '29.0', '60',3),
+(null, '2023-09-24 09:54:48', '28.3', '68.2',7),
+(null, '2023-09-24 11:04:20', '35', '45.8',5),
+(null, '2023-09-23 6:00:39', '33', '40',2),
+(null, '2023-09-23 08:30:00', '31', '64.6',4),
+(null, '2023-09-24 08:45:42', '34', '68.2',6);
 
-select dataHora as 'Horário',
-		temperatura as 'Temperatura(C°)',
-        umidade as ' umidade(%)'
-		from registro;
+select 
+idSensor as 'Sensor',		
+dataHora as 'Horário',
+temp as 'Temperatura(C°)',
+umid as ' umidade(%)'
+from sensorDHT11
+right join registro
+on fkSensor = idSensor;
 
--- adcicionei A foreign key a tabela usuario
-
-alter table usuario add column fkUempresa int,
-	add foreign key (fkUempresa)
-    references empresa(idEmpresa);
-    
-describe usuario;
-    
-update usuario set fkUempresa = 1
-	where idUsuario = 3;
-    
-update usuario set fkUempresa = 2
-	where idUsuario = 1; 
-    
-update usuario set fkUempresa = 3
-	where idUsuario = 2;
-    
-update usuario set fkUempresa = 4
-	where idUsuario = 4;
-	
-select empresa.nome as 'Empresa',
-		usuario.login as 'Encarregado',
-        usuario.senha as 'Senha',
-        usuario.cargo as 'Cargo'
-        from empresa join usuario
-        on idEmpresa = fkUempresa;
-
-
--- Adicionei a chave estrangeira na tabela colmeia
-
-alter table colmeia add column fkempresa int,
-	add foreign key(fkempresa)
-    references empresa(idEmpresa);
-
-update colmeia set fkempresa = 2
-	where idColmeia = 2;
-    
-update colmeia set fkempresa = 1
-	where idColmeia = 4;
-    
-update colmeia set fkempresa = 4
-	where idColmeia = 3;
-    
-update colmeia set fkempresa = 3
-	where idColmeia = 1;
-    
-select nome as 'empresa',
-		qtdColmeia as 'Colmeias',
-        tipoAbelha as 'Abelha'
-		from colmeia join empresa
-        on idEmpresa = fkempresa;
-
-alter table sensorDHT11 add column fkColmeia int,
-	add foreign key (fkColmeia)
-    references colmeia(idColmeia);
-    
-update sensorDHT11 set fkColmeia = 1
-	where idSensor in(1,2);
-    
-update sensorDHT11 set fkColmeia = 2
-	where idSensor in (7,8);
-
-update sensorDHT11 set fkColmeia = 3
-	where idSensor in (3,4);
-    
-update sensorDHT11 set fkColmeia = 4
-	where idSensor in (5,6);
-    
-select * from empresa 
-	inner join colmeia 
-	on idEmpresa = fkEmpresa
-    inner join sensorDHT11
-    on idColmeia = fkColmeia;
-    
-alter table registro add column fkSensor int,
-	add foreign key(fkSensor)
-	references sensorDHT11(idSensor);
-    
-update registro set fkSensor = 1
-	where idRegistro in(1,2);
-    
-update registro set fkSensor = 2
-	where idRegistro in(7,8);
-    
-update registro set fkSensor = 3
-	where idRegistro in(5,6);
-    
-update registro set fkSensor = 4
-	where idRegistro in(3,4);
-    
-select sensorDHT11.idSensor as 'Sensor',
-		registro.dataHora as 'Horário',
-		registro.temperatura as 'Temperatura(C°)',
-        registro.umidade as ' umidade(%)'
-		from registro join sensorDHT11
-        on idSensor = fkSensor;
+select 
+sen.idSensor as 'Sensor',
+r.temp as 'Temperatura',
+r.umid as 'Umidade',
+r.dataHora as 'Hórario',
+c.idColmeia as 'Colmeia',
+st.nomeFazenda as 'Fazenda',
+e.nomeFantasia as 'Empresa',
+e.uf as 'Estado'
+from registro as r
+join sensorDHT11 as sen
+on r.fkSensor = sen.idSensor
+inner join colmeia as c
+on sen.fkColmeia = c.idColmeia
+inner join setor as st
+on c.fkSetor = st.idSetor
+inner join empresa as e
+on st.fkEmpresa = e.idEmpresa
+where idEmpresa = 1002;
